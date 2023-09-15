2dcb60a98ffb8aca064bb1faa008e205f2716eb2
AbiR7171 <abiremtiag@gmail.com>
2023-09-16 03:16:38 +0600

    json created for show data

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Pages/Header';
 import SearchPage from '../Pages/SearchPage';
+import MainPage from '../Pages/MainPage';
 
 const MainLayout = () => {
     return (
         <div>
             <Header/>
             <SearchPage/>
+            <MainPage/>
             
         </div>
     );
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+const MainPage = () => {
+    return (
+        <div> 
+
+            <section className='container mx-auto px-32 mt-20 flex justify-around items-center gap-32'>
+ 
+                    <section className='flex gap-20'>
+                    <div className='hover:border-b-4 border-sky-700 text-black hover:text-sky-800'>
+                          <p className='text-4xl text-black hover:text-sky-800'><Icon icon="game-icons:love-song" /></p>
+                          <p className='font-primary  font-bold'>Classic</p>
+                     </div>
+                     <div className='hover:border-b-4 border-sky-700 text-black hover:text-sky-800'>
+                          <p className='text-4xl'><Icon icon="game-icons:love-song" /></p>
+                          <p className='font-primary font-bold'>Sharp</p>
+                     </div>
+                    
+                     <div className='hover:border-b-4 border-sky-700 text-black hover:text-sky-800'>
+                          <p className='text-4xl '><Icon icon="simple-icons:fontawesome" /></p>
+                          <p className='font-primary font-bold'>Brand</p>
+                     </div>
+                     <div className='hover:border-b-4 border-sky-700 text-black hover:text-sky-800'>
+                          <p className='text-4xl '><Icon icon="fontisto:arrow-expand" /></p>
+                          <p className='font-primary'>Free</p>
+                     </div>
+                    </section>
+                   
+                   <section className='flex gap-8 items-center'>
+
+                           
+                     <div className=''>
+
+                          <p className='text-3xl text-black hover:text-sky-800'><Icon icon="ep:menu" /></p>
+                        
+                     </div>
+                     <div className=''>
+                          <p className='text-3xl text-black hover:text-sky-800'><Icon icon="healthicons:ui-menu-grid" /></p>
+                       
+                     </div>
+                     <div className=''>
+                          <p className='text-3xl text-black hover:text-sky-800'><Icon icon="system-uicons:menu-hamburger" /></p>
+                         
+                     </div>
+                     <div className=''>
+
+                     <select className="select select-bordered w-full max-w-xs">
+                           <option disabled selected>Featured</option>
+                           <option>Alphabetical</option>
+                           </select>
+
+
+                        
+                     </div>
+                     <div className=''>
+
+                     <select className="select select-bordered w-full max-w-xs">
+                           <option disabled selected>6.4.2</option>
+                           <option>5.15.4</option>
+                           <option>All versions</option>
+                           </select>
+
+
+                        
+                     </div>
+
+                   </section>
+                  
+
+            </section> 
+
+
+            
+
+            
+        </div>
+    );
+};
+
+export default MainPage;
